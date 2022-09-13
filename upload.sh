c78c37f0e23814e38e5c94aa40e4ba678845d34c
arjit547 <97424724+arjit547@users.noreply.github.com>
2022-09-13 11:05:11 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu
 sudo apt-get update
-sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
 sudo pip install virtualenv
 virtualenv env
 source env/bin/activate
