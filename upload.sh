9c34688fd78dcb297c36d9a0ab7a292186238ff1
arjit547 <97424724+arjit547@users.noreply.github.com>
2022-09-28 15:28:44 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,8 @@
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
-sudo pip install virtualenv
+apt install virtualenv -y
+apt install python-pip -y
 virtualenv env
 source env/bin/activate
 pip3 install django
